a0eb87dd65473551be719f49ef857d90af753b6e
JereGanzz <84123521+JereGanzz@users.noreply.github.com>
2021-05-31 16:06:18 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Terkey Termux
+Terkey Termux
+
+# Install
+```
+$pkg install git -y
+$pkg install python -y
+$git clone https://github.com/JereGanzz/terkey
+$cd terkey
+```
+# Run
+```
+$ls
+$python terkey.py
+```
+
+# Sosial Media
+[`Github`](`https://github.com/JereGanzz`)
---
+++ terkey.py
@@ -0,0 +1,37 @@
+import os
+from time import sleep
+
+
+a ='\033[92m'
+b ='\033[91m'
+c ='\033[0m'
+
+def setup():
+    try:
+        os.mkdir('/data/data/com.termux/files/home/.termux')
+    except:
+        pass
+    key = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]"
+    open('/data/data/com.termux/files/home/.termux/termux.properties','w').write(key)
+    os.system('termux-reload-settings')
+
+
+def banner():
+    os.system('clear')
+    print(a+'Shortcut for help you'.center(40))
+    print(b+'Jere Ganzz'.center(40))
+    print("".join([i for i in "\n"*3]))
+
+
+if __name__=='__main__':
+    banner()
+    from threading import Thread as td
+    t = td(target=setup)
+    t.start()
+    while t.is_alive():
+        for i in "-\|/-\|/":
+            print('\rPlease wait '+i+' ',end="",flush=True)
+            sleep(0.1)
+    banner()
+    print(c+'Follow '+a+'https://github.com/JereGanzz'+c+' Woi')
+    
